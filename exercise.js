b21cf57ea59e099fb553eec316097a12893777ad
aconkli9 <aconkli9@ford.com>
2022-03-29 13:35:11 -0400

    Add solutions

--- exercise.js
+++ exercise.js
@@ -1,8 +1,31 @@
 // Your code goes here
 
+function getItemById(itemsList, itemID) {
+    return itemsList.find(item => {
+        return item.id === itemID
+    })
+}
+
+function buildTransactions(salesList, itemsList) {
+    return salesList.map(sale => {
+        return {
+            ...sale,
+            description: getItemById(itemsList, sale.itemId).description,
+            price: getItemById(itemsList, sale.itemId).price,
+        }
+    })
+}
+
+function getTransactionsByItemDescription (transactions, description) {
+    return transactions.filter (transaction => {
+        return transaction.description === description
+    })
+
+}
+
 module.exports = {
     // Uncomment these as you write them
-    // getItemById,
-    // buildTransactions,
-    // getTransactionsByItemDescription
+    getItemById,
+    buildTransactions,
+    getTransactionsByItemDescription
 }
